0920285f97e45963b9daf12a237c03c9d784cc1b
bully23 <bullypb23@gmail.com>
2020-06-01 22:32:42 +0200

    modified welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Yummi Pizza</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Yummi Pizza</title>
 
-        
-    </head>
-    <body>
-        <div class="root">
-            <h1>Random for now</h1>
-        </div>
-    </body>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+
+</head>
+
+<body>
+    <div class="root">
+
+    </div>
+</body>
+
 </html>
